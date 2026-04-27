40330f0a93e863519ece31f1eba7cf5c3b47d13f
2003myriam <lounesmyriam210@gmail.com>
2026-04-27 21:34:14 +0100

    modify  the componenet  TerminalSnippetViewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import TerminalSnippetViewer from "./components/TerminalSnipeetViewer/TerminalSnippetViewer";
+import CodeComponenets from "./components/CodeComponenets/CodeComponenets";
 
 function App() {
   return (
     <div>
-       <TerminalSnippetViewer  text={"hello world  const a=15 console.log(a)"}/>
+      <TerminalSnippetViewer command={" npm run dev "}/>
+      
       <Routes>
         <Route path="/" element={<h1>home page</h1>} />
       </Routes>
