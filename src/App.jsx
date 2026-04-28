f71759188978dc1ce49333a8cbc21c49d5ab2db4
Adelboy34 <Adelhouma123@icloud.com>
2026-04-28 19:29:37 +0100

    Restore flat hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import TerminalSnippetViewer from "./components/TerminalSnipeetViewer/TerminalSnippetViewer";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div>
        
       <Routes>
-        <Route path="/" element={<h1>home page</h1>} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+import Herosection from "../components/HeroSection/Herosection";
+
+function Homepage() {
+  return <Herosection />;
+}
+
+export default Homepage;
