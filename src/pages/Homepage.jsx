fdea06136c558eea2bbb9bb7fe89b5d56a0ffe40
Adelboy34 <Adelhouma123@icloud.com>
2026-04-29 10:52:01 +0100

    resolved another conflit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import Herosection from "../components/HeroSection/Herosection";
-=======
-import Herosection from "../components/herosection/Herosection";
->>>>>>> b4485e17d308292bea0c66c738b964d214104caf
 
 function Homepage() {
   return <Herosection />;
