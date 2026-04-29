fb5edc062fa8d0a28b07c930b9b3b2353b70b836
2003myriam <lounesmyriam210@gmail.com>
2026-04-29 11:09:59 +0100

    problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
  
 import IconeSlider from "./components/IconeSlider/IconeSlider";
+import TerminalSnippetViewer from "./components/TerminalSnipeetViewer/TerminalSnippetViewer";
+import CodeComponenets from "./components/CodeComponenets/CodeComponenets";
 function App() {
   return (
     <div>
       <IconeSlider/>
-       
+      <br />
+       <TerminalSnippetViewer/>
+       <br />
+       <CodeComponenets/>
       <Routes>
         <Route path="/" element={<h1>home page</h1>} />
       </Routes>
